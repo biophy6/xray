25b88e3281bcefef1f09616c621c100a78fcc30d
Omar Bakht <biophy6@yahoo.com>
2013-03-27 13:54:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xray::Application.config.secret_token = 'd6076ce80c71d1912fcb85f8e1615110e1163af262c43ee0b3717b6d99a3e6e3605852a626a342ef84cd5a37a0e675720a6515281085dcaf2f50c419d0da57dc'
